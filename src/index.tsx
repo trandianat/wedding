582a6f080b34bb765180761b6586ca16b14abc68
Diana Tran <trandianat@gmail.com>
2022-03-28 21:17:54 -0400

    Added Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const HelloWorld = () => {
-    return (
-        <h1>
-            Hello World!
-        </h1>
-    );
-}
+  return <h1>Hello World!</h1>;
+};
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
