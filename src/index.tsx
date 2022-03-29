782823b224f663f09658767d23033e5b82f3c8a3
Diana Tran <trandianat@gmail.com>
2022-03-28 21:45:07 -0400

    Added implicit return rule

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const HelloWorld = () => {
-  return <h1>Hello World!</h1>;
-};
+const HelloWorld = () => <h1>Hello World!</h1>;
 
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
