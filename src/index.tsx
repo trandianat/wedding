b947b8f22366a4483976ee925bbee8031ec6bc7c
Diana Tran <trandianat@gmail.com>
2022-03-28 22:30:06 -0400

    Moved main component to its own file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
 
-const HelloWorld = () => <h1>Hello World!</h1>;
-
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
