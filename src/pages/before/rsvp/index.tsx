4ff2329388d8988f89d917eedf8dbb0432d8b812
Diana Tran <trandianat@gmail.com>
2022-09-21 18:29:37 -0400

    Build test

--- src/pages/before/rsvp/index.tsx
+++ src/pages/before/rsvp/index.tsx
@@ -1,20 +1,4 @@
-import { useEffect } from 'react';
-import { API } from 'aws-amplify';
-import { listContents } from 'graphql/queries';
-
 export const RSVP = (): JSX.Element => {
-  const fetchDetails = async () => {
-    const apiData: any = await API.graphql({ query: listContents });
-    console.log(
-      'apiData',
-      JSON.parse(apiData.data.listContents.items[0].content)
-    );
-  };
-
-  useEffect(() => {
-    fetchDetails();
-  }, []);
-
   return (
     <main>
       <h3>RSVP</h3>
