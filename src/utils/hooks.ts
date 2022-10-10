0e7c967456942146b7aff6e306ae41eecf60203e
Diana Tran <trandianat@gmail.com>
2022-10-10 17:59:15 -0400

    Wait for API result

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -7,15 +7,16 @@ export const useData = (category: Category) => {
     const [data, setData] = useState<any>(null);
 
     useEffect(() => {
-        console.log('data', data);
         const getData = async () => {
             const result: any = await API.graphql({
                 query: listContents,
                 variables: { filter: { category: { eq: category } } },
             });
-            setData(JSON.parse(result.data.listContents.items[0].content));
+            console.log('result', result);
+            if (result) setData(JSON.parse(result.data.listContents.items[0].content));
         };
         if (!data) getData();
+        console.log('data', data);
     }, [data]);
 
     return data;
