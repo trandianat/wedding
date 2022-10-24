6b2cc395740ba338204e3842bb81d0c21091a529
Diana Tran <trandianat@gmail.com>
2022-10-23 20:28:13 -0400

    Added story content

--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -1,3 +1,8 @@
 import { css } from '@emotion/react';
 
-export const footer = css({ fontSize: '.875rem', padding: 32, textAlign: 'center' });
+export const footer = css({
+    fontSize: '.875rem',
+    padding: 32,
+    textAlign: 'center',
+    a: { whiteSpace: 'nowrap' }
+});
--- src/pages/before/story/index.tsx
+++ src/pages/before/story/index.tsx
@@ -13,31 +13,21 @@ export const Story = (): JSX.Element => {
           <h2>The beginning</h2>
           <Divider variant={Variant.SECONDARY} />
           {data.beginning.map((paragraph: string) => (
-            <p>{paragraph}</p>
+            <p key={paragraph}>{paragraph}</p>
           ))}
         </Background>
         <Background variant={Variant.SECONDARY}>
           <h2>The relationship</h2>
           <Divider />
-          <p>
-            Brendan and Diana made their relationship official on a foggy
-            Thanksgiving night in 2016. They never talked about long-term plans
-            together.
-          </p>
-          <p>
-            Brendan knew Diana was the one when she trekked through mud with him
-            to see his favorite band, Metallica, at Slane Castle in Ireland.
-          </p>
-          <p>
-            In the middle of the pandemic, they decided to buy a place together
-            and it dawned on them that this was probably serious.
-          </p>
+          {data.relationship.map((paragraph: string) => (
+            <p key={paragraph}>{paragraph}</p>
+          ))}
         </Background>
         <Background>
           <h2>The engagement</h2>
           <Divider />
           {data.engagement.map((paragraph: string) => (
-            <p>{paragraph}</p>
+            <p key={paragraph}>{paragraph}</p>
           ))}
         </Background>
       </main>
