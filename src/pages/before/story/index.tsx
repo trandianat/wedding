7ae339fb7bd4d72f3c062710417acb41a04fcd9f
Diana Tran <trandianat@gmail.com>
2022-10-23 22:01:14 -0400

    Added polaroid

--- src/pages/before/story/index.tsx
+++ src/pages/before/story/index.tsx
@@ -1,20 +1,32 @@
 import { Background } from 'components/background';
 import { Divider } from 'components/divider';
 import * as styles from 'pages/before/story/styles';
-import { useData } from 'utils/hooks';
+import { useData, useImages } from 'utils/hooks';
 import { Category, Variant } from 'utils/types';
 
 export const Story = (): JSX.Element => {
   const data = useData(Category.STORY);
+  const [halloween, engagement] = useImages(Category.STORY);
   return (
     data && (
       <main css={styles.story}>
         <Background variant={Variant.PRIMARY}>
           <h2>The beginning</h2>
           <Divider variant={Variant.SECONDARY} />
-          {data.beginning.map((paragraph: string) => (
-            <p key={paragraph}>{paragraph}</p>
-          ))}
+          <div css={styles.content}>
+            <div>
+              {data.beginning.map((paragraph: string) => (
+                <p key={paragraph}>{paragraph}</p>
+              ))}
+            </div>
+            <div css={styles.polaroid}>
+              <img src={halloween} />
+              <p>
+                Used laundry hampers to dress up as our favorite beers for
+                Halloween at the Hurleys' in 2017
+              </p>
+            </div>
+          </div>
         </Background>
         <Background variant={Variant.SECONDARY}>
           <h2>The relationship</h2>
@@ -26,9 +38,14 @@ export const Story = (): JSX.Element => {
         <Background>
           <h2>The engagement</h2>
           <Divider />
-          {data.engagement.map((paragraph: string) => (
-            <p key={paragraph}>{paragraph}</p>
-          ))}
+          <div css={{ display: 'flex', gap: 16 }}>
+            <img css={{ height: 350 }} src={engagement} />
+            <div>
+              {data.engagement.map((paragraph: string) => (
+                <p key={paragraph}>{paragraph}</p>
+              ))}
+            </div>
+          </div>
         </Background>
       </main>
     )
--- src/pages/before/story/styles.ts
+++ src/pages/before/story/styles.ts
@@ -1,3 +1,35 @@
+import { css } from '@emotion/react';
+
 export const story = {
     'p:not(:last-child)': { marginBottom: 12 }
 };
+
+export const content = css({
+    display: 'flex',
+    gap: 24,
+    '@media (max-width: 950px)': { flexDirection: 'column' },
+});
+
+export const polaroid = css({
+    alignSelf: 'center',
+    backgroundColor: '#EFEFEF',
+    borderRadius: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 16,
+    height: 'fit-content',
+    padding: 24,
+    width: 'fit-content',
+    img: {
+        height: 325,
+        objectFit: 'cover',
+        objectPosition: 'top',
+        width: 325,
+    },
+    p: {
+        color: 'var(--black)',
+        fontSize: '.875rem',
+        lineHeight: 1.2,
+        width: 325,
+    }
+});
