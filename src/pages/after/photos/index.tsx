1cb85e1cb8d865ddb09710aee3b17247b7435e5f
Diana Tran <trandianat@gmail.com>
2022-10-24 20:36:50 -0400

    Added temporary message for photos

--- src/pages/after/photos/index.tsx
+++ src/pages/after/photos/index.tsx
@@ -1,1 +1,13 @@
-export const Photos = (): JSX.Element => <h2>Photos</h2>;
+import { Background } from 'components/background';
+import { Divider } from 'components/divider';
+import { Variant } from 'utils/types';
+
+export const Photos = (): JSX.Element => (
+  <main>
+    <Background variant={Variant.PRIMARY}>
+      <h2>Photos</h2>
+      <Divider variant={Variant.SECONDARY} />
+      <p>Stay tuned for photos of the wedding!</p>
+    </Background>
+  </main>
+);
