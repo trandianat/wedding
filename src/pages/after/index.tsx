266de8db20e2549198ef157b6f1874b3686bc4e0
Diana Tran <trandianat@gmail.com>
2023-02-16 16:43:30 -0500

    Added Outlet to before and after routes

--- src/pages/after/index.tsx
+++ src/pages/after/index.tsx
@@ -1,15 +1,19 @@
+import { Fragment } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import { Header } from 'components/header';
-import { Route, Routes } from 'react-router-dom';
 import { Photos } from 'pages/after/photos';
 import { Thanks } from 'pages/after/thanks';
 import { Vendors } from 'pages/after/vendors';
 
 export const After = (): JSX.Element => (
-  <Routes>
-    <Route element={<Header />} path="/">
-      <Route element={<Photos />} path="photos" />
-      <Route element={<Thanks />} path="thanks" />
-      <Route element={<Vendors />} path="vendors" />
-    </Route>
-  </Routes>
+  <Fragment>
+    <Routes>
+      <Route element={<Header />} path="/">
+        <Route element={<Photos />} path="photos" />
+        <Route element={<Thanks />} path="thanks" />
+        <Route element={<Vendors />} path="vendors" />
+      </Route>
+    </Routes>
+    <Outlet />
+  </Fragment>
 );
--- src/pages/before/index.tsx
+++ src/pages/before/index.tsx
@@ -1,17 +1,21 @@
+import { Fragment } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import { Header } from 'components/header';
-import { Route, Routes } from 'react-router-dom';
 import { Activities } from 'pages/before/activities';
 import { Logistics } from 'pages/before/logistics';
 import { Party } from 'pages/before/party';
 import { Story } from 'pages/before/story';
 
 export const Before = (): JSX.Element => (
-  <Routes>
-    <Route element={<Header />} path="/">
-      <Route element={<Activities />} path="activities" />
-      <Route element={<Logistics />} path="logistics" />
-      <Route element={<Party />} path="party" />
-      <Route element={<Story />} path="story" />
-    </Route>
-  </Routes>
+  <Fragment>
+    <Routes>
+      <Route element={<Header />} path="/">
+        <Route element={<Activities />} path="activities" />
+        <Route element={<Logistics />} path="logistics" />
+        <Route element={<Party />} path="party" />
+        <Route element={<Story />} path="story" />
+      </Route>
+    </Routes>
+    <Outlet />
+  </Fragment>
 );
