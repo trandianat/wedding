e3b2e988ca4dc6786fc71d60d2e98184edef18ce
Diana Tran <trandianat@gmail.com>
2023-08-08 20:08:37 -0400

    Moved vendor data to DynamoDB

--- src/pages/after/vendors/index.tsx
+++ src/pages/after/vendors/index.tsx
@@ -6,31 +6,24 @@ import { Grid } from 'components/grid';
 import { Link } from 'components/link';
 import * as styles from 'pages/after/vendors/styles';
 import after from 'utils/after';
-import { useImages } from 'utils/hooks';
+import { useData, useImages } from 'utils/hooks';
 import { Category, Variant } from 'utils/types';
 
 export const Vendors = (): JSX.Element => {
+  const data = useData(Category.VENDORS) ?? {};
   const [engagement] = useImages(Category.VENDORS);
   const vendors = [
     {
       category: 'Venue',
-      description: (
-        <Link url="https://www.bedfordvillageinn.com">Bedford Village Inn</Link>
-      ),
+      description: <Link url={data.venueLink}>{data.venue}</Link>,
     },
     {
       category: 'Photography',
-      description: (
-        <Link url="https://klenoxphoto.com">K. Lenox Photography</Link>
-      ),
+      description: <Link url={data.photographyLink}>{data.photography}</Link>,
     },
     {
       category: 'Room block',
-      description: (
-        <Link url="https://www.hilton.com/en/hotels/mhtbfhx-hampton-suites-manchester-bedford">
-          Hampton Inn & Suites Manchester-Bedford
-        </Link>
-      ),
+      description: <Link url={data.roomBlockLink}>{data.roomBlock}</Link>,
     },
   ];
   if (after)
@@ -38,85 +31,69 @@ export const Vendors = (): JSX.Element => {
       ...[
         {
           category: 'Bus',
-          description: (
-            <Link url="http://blacktielimo.com">Black Tie Limousine</Link>
-          ),
+          description: <Link url={data.busLink}>{data.bus}</Link>,
         },
         {
           category: 'DJ',
           description: (
             <Fragment>
-              <Link url="http://joebermudez.com">Joe Bermudez</Link> of{' '}
-              <Link url="https://617weddings.com">617 Weddings</Link>
+              <Link url={data.djLink}>{data.dj}</Link> of{' '}
+              <Link url={data.djCompanyLink}>{data.djCompany}</Link>
             </Fragment>
           ),
         },
         {
           category: 'Flowers',
-          description: (
-            <Link url="https://apothecaflowershoppe.com">Apotheca Flowers</Link>
-          ),
+          description: <Link url={data.flowersLink}>{data.flowers}</Link>,
         },
         {
           category: 'Hair and makeup',
-          description: (
-            <Link url="http://www.luxebvi.com">Luxe Salon & Spa</Link>
-          ),
+          description: <Link url={data.salonLink}>{data.salon}</Link>,
         },
         {
           category: 'Photo booth',
-          description: (
-            <Link url="https://photoboothplanet.com">Photo Booth Planet</Link>
-          ),
-        },
-        {
-          category: 'Tailor',
-          description: (
-            <Link url="https://www.yelenacouture.com">
-              Yelena Couture Tailor
-            </Link>
-          ),
+          description: <Link url={data.photoBoothLink}>{data.photoBooth}</Link>,
         },
         {
           category: 'Videographer',
           description: (
-            <Link url="https://www.hindsightweddingfilms.net">
-              Hindsight Wedding Films
-            </Link>
+            <Link url={data.videographyLink}>{data.videography}</Link>
           ),
         },
         {
           category: 'Wedding dress',
           description: (
             <Fragment>
-              <Link url="https://nettabenshabu.com">Netta BenShabu</Link> at{' '}
-              <Link url="https://flairbridesmaid.com">Flair Boston</Link>
+              <Link url={data.dressBrandLink}>{data.dressBrand}</Link> at{' '}
+              <Link url={data.dressShopLink}>{data.dressShop}</Link>
             </Fragment>
           ),
         },
       ]
     );
   return (
-    <main css={styles.vendors}>
-      <Background variant={Variant.SECONDARY}>
-        <h2>Our vendors</h2>
-        <Divider />
-        {after ? (
-          <p>
-            We wouldn't have been able to pull off our wedding without our
-            amazing team of vendors:
-          </p>
-        ) : (
-          <p>
-            After our wedding, the rest of our team of vendors will be listed
-            below. We are so thankful for all of the work that they have been
-            putting into planning and executing our wedding!
-          </p>
-        )}
-        <Grid items={vendors} spacing={1} />
-        <img src={engagement} />
-      </Background>
-      <Footer />
-    </main>
+    data && (
+      <main css={styles.vendors}>
+        <Background variant={Variant.SECONDARY}>
+          <h2>Our vendors</h2>
+          <Divider />
+          {after ? (
+            <p>
+              We wouldn't have been able to pull off our wedding without our
+              amazing team of vendors:
+            </p>
+          ) : (
+            <p>
+              After our wedding, the rest of our team of vendors will be listed
+              below. We are so thankful for all of the work that they have been
+              putting into planning and executing our wedding!
+            </p>
+          )}
+          <Grid items={vendors} spacing={1} />
+          <img src={engagement} />
+        </Background>
+        <Footer />
+      </main>
+    )
   );
 };
